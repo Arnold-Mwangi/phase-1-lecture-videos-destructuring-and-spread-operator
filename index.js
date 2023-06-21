a04314b61bd49b4f04e6f0577167e957a33a4e8a
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-06-21 13:44:34 +0300

    destructing

---
+++ index.js
@@ -0,0 +1,11 @@
+const names = ["Arnold", "Mwangi", "Kirigwi"];
+console.log(names)
+// spread operator to create a new array
+ const newNames = [...names, "meArnoh"]
+console.log(newNames)
+
+//desctructing
+let [name1, name2, name3] = names;
+console.log(name1)
+console.log(name2)
+console.log(name3)
